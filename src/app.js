2022b5acd51d7b524e5556fd9395d2cca49111ff
BehramBakhtKhan <behrambakhtkhan22@gmail.com>
2026-09-17 23:55:01 +0500

    Implement tests, CI and CD

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.route.js';
 import securityMiddleware from '#middleware/security.middleware.js';
-// import usersRoutes from '#routes/users.routes.js';
+import usersRoutes from '#routes/users.routes.js';
 
 const app = express();
 
@@ -43,7 +43,7 @@ app.get('/api', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);
-// app.use('/api/users', usersRoutes);
+app.use('/api/users', usersRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Route not found' });
